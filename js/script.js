37c41c38892ebe8d85329f7e5169bf5c6026f423
Manan17 <shahmanan170602@gmail.com>
2023-01-15 19:28:14 +0530

    ddsdsdsadsdsadasd

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let minItem = document.querySelector("#min");
 let secItem = document.querySelector("#sec");
 
 let countDown = () => {
-  let futureDate = new Date("6 Feb 2023");
+  let futureDate = new Date("9 Feb 2023");
   let currentDate = new Date();
   let myDate = futureDate - currentDate;
   //console.log(myDate);
